14e1d47033d94d9b9d399d70bfcd5688846381c8
rdisho20 <xnod_bs@yahoo.com>
2025-08-07 18:24:11 -0600

    adding cms files

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import g, Flask, render_template, request, send_from_directory
+from flask import (
+    flash,
+    Flask,
+    g,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    session,
+    url_for,
+)
+
+from markdown import markdown
+from werkzeug.exceptions import NotFound
 import os
 
 app = Flask(__name__)
+app.secret_key = 'secret1'
 
 @app.before_request
 def get_data_directory():
@@ -14,9 +28,49 @@ def index():
     files = [os.path.basename(path) for path in os.listdir(g.data_dir)]
     return render_template('data.html', files=files)
 
-@app.route('/<file>')
-def download_file(file):
-    return send_from_directory(g.data_dir, file)
+@app.route('/<filename>')
+def download_file(filename):
+    file_path = os.path.join(g.data_dir, filename)
+
+    if os.path.isfile(file_path):
+        if filename.endswith('.md'):
+            with open(file_path, 'r') as file:
+                return markdown(file.read())
+        else:
+            return send_from_directory(g.data_dir, filename)
+    
+    else:
+        flash(f"{filename} not found", "error")
+        return redirect(url_for('index'))
+
+@app.route('/<filename>/edit')
+def edit_file(filename):
+    file_path = os.path.join(g.data_dir, filename)
+
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return render_template('edit.html', filename=filename, content=content)
+    else:
+        flash(f"{filename} not found")
+        return redirect(url_for('index'))
+
+@app.route('/<filename>', methods=['POST'])
+def save_changes(filename):
+    changes = request.form["content"]
+    file_path = os.path.join(g.data_dir, filename)
+    with open(file_path, 'w') as file:
+        file.write(changes)
+
+    flash(f"{filename.upper()} has been updated!", "success")
+    return redirect(url_for('index'))
+
+'''
+@app.errorhandler(404)
+def file_not_found(error):
+    session.modified = True
+    return redirect(url_for('index'), 302)
+'''
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,26 +7,40 @@ class CMSTest(unittest.TestCase):
         self.client = app.test_client()
 
     def test_index(self):
+        # returning response object (html) so don't need 'with'
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
         self.assertIn("about.txt", response.get_data(as_text=True))
         self.assertIn("changes.txt", response.get_data(as_text=True))
         self.assertIn("history.txt", response.get_data(as_text=True))
 
-    def test_download_file(self):
-        response = self.client.get("/about.txt")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("This is my app.", response.get_data(as_text=True))
+    def test_viewing_text_document(self):
+        with self.client.get("/about.txt") as response:
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.content_type, "text/plain; charset=utf-8")
+            self.assertIn("This is my app.", response.get_data(as_text=True))
 
-    def test_file_nonexistent(self):
+    def test_document_not_found(self):
+        # Attempt to access a nonexistent file and verify a redirect happens
         with self.client.get("/test.txt") as response:
             self.assertEqual(response.status_code, 302)
 
+        # Verify redirect and message handling works
         with self.client.get(response.headers['Location']) as response:
             self.assertEqual(response.status_code, 200)
-            self.assertIn("text.txt does not exist",
+            self.assertIn("test.txt not found",
                           response.get_data(as_text=True))
         
+        # Assert that a page reload removes the message
         with self.client.get("/") as response:
-            self.assertNotIn("text.txt does not exist",
+            self.assertNotIn("test.txt not found",
                              response.get_data(as_text=True))
+
+    def test_markdown_file(self):
+        # returning response object (html) so don't need 'with'
+        response = self.client.get("/python_is.md")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+        self.assertIn("<h1>Python is...</h1>",
+                        response.get_data(as_text=True))
